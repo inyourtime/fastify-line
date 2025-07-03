4faca59ccce026724bd05c0f00497887446735f8
inyourtime <inyourtimeguy@gmail.com>
2025-07-03 22:28:47 +0700

    jsdoc

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,10 @@ import { kRawBody } from './symbols.js'
 
 declare module 'fastify' {
   interface FastifyInstance {
+    /**
+     * The LINE Messaging API client instance.
+     * This client is used to send messages and interact with the LINE platform.
+     */
     line: messagingApi.MessagingApiClient
   }
 
@@ -11,11 +15,24 @@ declare module 'fastify' {
   }
 
   interface FastifyContextConfig {
+    /**
+     * Whether to enable LINE webhook handling for this route.
+     * If set to `true`, the plugin will automatically parse the raw body and verify the
+     * signature of incoming requests.
+     */
     lineWebhook?: boolean
   }
 }
 
 export interface FastifyLineOptions {
+  /**
+   * The channel secret of your LINE Messaging API channel.
+   * This is used to verify the signature of incoming requests.
+   */
   channelSecret: string
+  /**
+   * The channel access token of your LINE Messaging API channel.
+   * This is used to send messages and interact with the LINE platform.
+   */
   channelAccessToken: string
 }
